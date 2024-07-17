1f061a875b565e2fe115b6901a65de7ee16efa0f
RudyDev-engfr <rudy.prevaud@gmail.com>
2024-07-17 15:19:16 +0200

    Remove useEffect conflicting in layoutjs

--- app/layout.js
+++ app/layout.js
@@ -1,43 +1,19 @@
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
-import * as gtag from "../lib/gtag";
+// app/layout.js
+import GoogleAnalytics from "../lib/GoogleAnalytics";
+import "./globals.css";
+
+const GA_TRACKING_ID = "YOUR_GA_TRACKING_ID";
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-
-  useEffect(() => {
-    const handleRouteChange = url => {
-      gtag.pageview(url);
-    };
-
-    handleRouteChange(pathname);
-    window.addEventListener("hashchange", e => handleRouteChange(window.location.pathname));
-    window.addEventListener("popstate", e => handleRouteChange(window.location.pathname));
-
-    return () => {
-      window.removeEventListener("hashchange", handleRouteChange);
-      window.removeEventListener("popstate", handleRouteChange);
-    };
-  }, [pathname]);
-
   return (
-    <>
+    <html>
       <head>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-            `,
-          }}
-        />
+        <title>Etablissement Rouquet</title>
       </head>
-      {children}
-    </>
+      <body>
+        {children}
+        <GoogleAnalytics GA_TRACKING_ID={GA_TRACKING_ID} />
+      </body>
+    </html>
   );
 }
